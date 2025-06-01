1bf55078f2da9175a00091893d14936de851b1a7
montmaj <timelord1102@gmail.com>
2025-06-01 00:52:42 -0400

    Mixin fix

--- src/main/java/com/perfectparitypg/mixin/FlowerPotMixin.java
+++ src/main/java/com/perfectparitypg/mixin/FlowerPotMixin.java
@@ -8,45 +8,37 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FlowerPotBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FlowerPotBlock.class)
-public class FlowerPotMixin extends Block {
-
-	@Final
-	@Shadow
-	private Block potted;
-
-	public FlowerPotMixin(Properties properties) {
-		super(properties);
-	}
+@Mixin(BlockBehaviour.class)
+public class FlowerPotMixin {
 
 	@Unique
 	protected boolean isRandomlyTicking(BlockState blockState) {
 		return blockState.is(ModBlocks.POTTED_OPEN_EYEBLOSSOM) || blockState.is(ModBlocks.POTTED_CLOSED_EYEBLOSSOM);
 	}
 
-	@Override
-	protected void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
-		if (this.isRandomlyTicking(blockState) && serverLevel.dimensionType().natural()) {
-			boolean bl = this.potted == ModBlocks.OPEN_EYEBLOSSOM;
-			boolean bl2 = CreakingHeartBlock.isNaturalNight(serverLevel);
-			if (bl != bl2) {
-				serverLevel.setBlock(blockPos, this.opposite(blockState), 3);
-				EyeblossomBlock.Type type = EyeblossomBlock.Type.fromBoolean(bl).transform();
-				type.spawnTransformParticle(serverLevel, blockPos, randomSource);
-				serverLevel.playSound((Entity)null, blockPos, type.longSwitchSound(), SoundSource.BLOCKS, 1.0F, 1.0F);
+	@Inject(method = "randomTick", at = @At("HEAD"))
+	protected void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource, CallbackInfo ci) {
+		if (blockState.getBlock() instanceof FlowerPotBlock && this.isRandomlyTicking(blockState) && serverLevel.dimensionType().natural()) {
+			if (blockState.getBlock() instanceof FlowerPotBlock) {
+				boolean bl = ((FlowerPotBlock) blockState.getBlock()).getPotted() == ModBlocks.OPEN_EYEBLOSSOM;
+				boolean bl2 = CreakingHeartBlock.isNaturalNight(serverLevel);
+				if (bl != bl2) {
+					serverLevel.setBlock(blockPos, this.opposite(blockState), 3);
+					EyeblossomBlock.Type type = EyeblossomBlock.Type.fromBoolean(bl).transform();
+					type.spawnTransformParticle(serverLevel, blockPos, randomSource);
+					serverLevel.playSound((Entity) null, blockPos, type.longSwitchSound(), SoundSource.BLOCKS, 1.0F, 1.0F);
+				}
 			}
 		}
-
-		super.randomTick(blockState, serverLevel, blockPos, randomSource);	}
-
+	}
 	@Unique
 	public BlockState opposite(BlockState blockState) {
 		if (blockState.is(ModBlocks.POTTED_OPEN_EYEBLOSSOM)) {
